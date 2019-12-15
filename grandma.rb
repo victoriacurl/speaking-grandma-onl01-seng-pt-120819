4ef0349a5e8b4e2f0b349ef4dd3d550273f359b9
Victoria Curl <victoriacurl9@gmail.com>
2019-12-15 16:20:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,10 @@
-# Write a speak_to_grandma method.
-
+def speak_to_grandma(string)
+  if string != string.uppercase
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif string.uppercase
+    return "NO, NOT SINCE 1938!"
+  end
+  
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
