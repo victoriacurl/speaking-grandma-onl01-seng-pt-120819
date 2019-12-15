8843480724cba35350584f4fe9992c1307a863dd
Victoria Curl <victoriacurl9@gmail.com>
2019-12-15 16:20:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,12 +4,12 @@ def speak_to_grandma(string)
   elsif string.uppercase
     return "NO, NOT SINCE 1938!"
   end
-  
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
